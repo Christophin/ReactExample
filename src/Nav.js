d041491a4839bc863aaf8346ad611c4261366c52
Chris Coralli <Ccoralli@gmail.com>
2022-07-11 11:38:41 -0400

    practice from scrimba react with bob ziroll

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Nav() {
+    return (
+      <div className="Nav">
+        <h1>Chris' Website</h1>
+        <ul>
+          <li>Home</li>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    );
+}
+
+export default Nav;
---
+++ src/StaticMarkup.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from './logo.svg';
+
+function StaticMarkup() {
+  return (
+    <div className="StaticMarkup">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Fun facts about react</h1>
+      <ul>
+        <li>Was first realeased in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+export default StaticMarkup
