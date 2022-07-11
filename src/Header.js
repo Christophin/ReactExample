e2500fee446cd5dd3c2e8a89ef3aa1af724be334
Chris Coralli <Ccoralli@gmail.com>
2022-07-11 17:24:30 -0400

    basic styling, moved nav to header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import Nav from './Nav'
+import logo from './logo.svg'
 
 function Header() {
-  return (
-    <header>
-      <Nav />
-    </header>
-  )
+    return (
+      <div className="Header">
+        <img className="header-img" src={logo} alt='logo'/>
+        <ul className="header-items">
+          <li>Home</li>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    );
 }
 
 export default Header
