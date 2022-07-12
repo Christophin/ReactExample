ed0363b52afe43d289703880e1e817d7f56630e0
Chris Coralli <Ccoralli@gmail.com>
2022-07-12 09:18:26 -0400

    moves images to folder

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 
 function MainContent() {
   return (
     <div className="MainContent App-default" >
       <img src={logo} alt="logos" width="80px" />
       <h1 float="right">Fun facts about react</h1>
-      <ol>
+      <ul>
         <li>Was first realeased in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ol>
+      </ul>
     </div>
   );
 }
