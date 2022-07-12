7b193cb7d815d6e0ef8e6828fc17256b8da06e48
Chris Coralli <Ccoralli@gmail.com>
2022-07-12 15:06:07 -0400

    Moved components to components folder. styled nav a bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import logo from './images/logo.svg'
+import logo from '../images/logo.svg'
 
 function Header() {
     return (
       <div className="Header">
         <img className="header-img" src={logo} alt='logo'/>
+        <h1 className="header-title">React Facts</h1>
         <ul className="header-items">
           <li>Home</li>
           <li>Pricing</li>
