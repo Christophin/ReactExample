0a887604521acb7dc806a5d8bbb580a56a6e24bb
Chris Coralli <Ccoralli@gmail.com>
2022-07-13 11:25:48 -0400

    finishing styling for static page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './components/Header'
-import MainContent from './components/MainContent'
+import Main from './components/Main'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainContent />
+      <Main />
       <Footer />
     </div>
   );
